e9738202e0439dc1c97606b8871347be0dc3cd7e
Thiago-Almeida23 <thiagocldealmeida@gmail.com>
2023-09-10 20:56:17 -0300

    content upgraded

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "picCover": "https://www.denofgeek.com/wp-content/uploads/2020/06/star-wars-squadrons-hera.jpeg?fit=2400%2C1350",
+    "title": "Phoenix Squadron Leader becomes General of the Rebel Alliance",
+    "description": "Hera Syndulla, the courageous leader of the Phoenix Squad, finally received the recognition she deserved. After years of leadership and heroism in the fight against the Galactic Empire, Hera was promoted to the rank of general in the Rebel Alliance. Her strategic prowess and unwavering dedication to the cause of freedom inspired her teammates, making her an icon of resistance and hope in a galaxy far, far away."
+  },
+  {
+    "id":"2",
+    "picCover": "https://qph.cf2.quoracdn.net/main-qimg-54127ee8b0d309d912d659d1936925fc.webp",
+    "title": "Too slow: Darth Maul challenges Obi-Wan after years and fares poorly.",
+    "description": "description of the news"
+  },
+  {
+    "id":"3",
+    "picCover": "https://gamespot.com/a/uploads/original/1578/15789737/3359866-chopper.jpg",
+    "title": "Short-fused robot: Chopper's already killed over 500,000. Hero or villain?",
+    "description": "description of the news"
+  },
+  {
+    "id":"4",
+    "picCover": "https://preview.redd.it/i-wish-lego-had-made-a-season-4-version-minifigure-of-ezra-v0-kge74z46ei981.jpg?width=1280&format=pjpg&auto=webp&s=f5c6ac4550659f73069a413b0fcf87aa737d115a",
+    "title": "Check out Ezra's helmets collection: Compulsive hoarder or just a collector?",
+    "description": "description of the news"
+  }
+]
+
